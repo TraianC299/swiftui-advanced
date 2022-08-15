d641f868b79b52d4d6955ef4c13b62ec709b7f02
TraianC299 <traianciochina@gmail.com>
2022-08-15 16:06:13 +0300

    Initial Commit

---
+++ AdvancedTutorial/AdvancedTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedTutorialApp.swift
+//  AdvancedTutorial
+//
+//  Created by Traian on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
