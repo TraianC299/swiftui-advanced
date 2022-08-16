e759b1d5851ba7a9e07f1c19d0f20a57b50ff87f
TraianC299 <traianciochina@gmail.com>
2022-08-16 17:12:34 +0300

    added multiple sheets example

--- AdvancedTutorial/MultipleSheetsExample.swift
+++ AdvancedTutorial/MultipleSheetsExample.swift
@@ -7,9 +7,40 @@
 
 import SwiftUI
 
+
+struct RandomModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+}
 struct MultipleSheetsExample: View {
+    @State var selectedModel:RandomModel? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                ForEach(1..<20) { index in
+                    CustomButton(title: "Button\(index)") {
+                        selectedModel = RandomModel(title: "TITLE \(index)")
+                    }
+                }
+              
+            }
+        }
+        
+        //the content for sheet is created as soon as the scrren loads
+        //this completion of sheet will trigger the sheet every time the selectedModel changes
+        .sheet(item: $selectedModel) { model in
+            NextScreen(selectedModel: model)
+        }
+    }
+}
+
+struct NextScreen: View{
+    let selectedModel:RandomModel
+    var body: some View {
+        Text(selectedModel.title)
+            .font(.largeTitle)
+            .foregroundColor(.red)
     }
 }
 
